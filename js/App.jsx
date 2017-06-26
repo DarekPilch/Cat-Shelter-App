2a72463e06fe008cd487940810d623d9adcdbca9
Darek Pilch <d.pilch31@gmail.com>
2017-06-26 12:01:06 +0200

    React project

---
+++ js/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render () {
+        return <section>
+            <header>
+                <form>
+                    <div><label><input type="text"/></label></div>
+                    <div><label><input type="checkbox"/> Only show cats that like kids</label></div>
+                </form>
+            </header>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th colSpan="2">male</th>
+                    </tr>
+                    <tr>
+                        <td>Fidel</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>Fidel</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <th colSpan="2">female</th>
+                    </tr>
+                    <tr>
+                        <td>Yude</td>
+                        <td>4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    }
+}
+
+export default App;
